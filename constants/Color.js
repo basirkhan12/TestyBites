9baefba159b049034d58b2818be850ed8741f82c
basiru004 <basiru004@gmail.com>
2023-06-10 16:11:15 +0500

    added react navigation, navigation stake, home sceen

---
+++ constants/Color.js
@@ -0,0 +1,7 @@
+export const Colors = {
+    primary: '#0A2233', // example primary color
+    secondary: '#FFFFFF', // example secondary color
+    text: '#030319', // example text color
+    background: '#F7F7F7', // example background color
+
+};
---
+++ screens/Home.js
@@ -0,0 +1,26 @@
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+// create a component
+const Home = () => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text>Home</Text>
+        </SafeAreaView>
+    );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#2c3e50',
+    },
+});
+
+//make this component available to the app
+export default Home;
